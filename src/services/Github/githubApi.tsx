8d0e0cb6b8dc502fce7f887ccdbd08d0a4005907
Evandro Cantelmo <evandrocantelmo@gmail.com>
2022-12-11 00:42:31 -0300

    feat:sort in api response

--- src/services/Github/githubApi.tsx
+++ src/services/Github/githubApi.tsx
@@ -14,6 +14,10 @@ export async function GithubRepoApi({ User }: Props) {
   try {
     const response = await fetch(`https://api.github.com/users/${User}/repos`);
     const data = await response.json();
+    data?.sort(
+      (a: any, b: any) =>
+        b?.forks + b?.stargazers_count - (a?.forks_count + a?.stargazers_count)
+    );
 
     return { error: false, data: data, status: response.status };
   } catch (error) {
